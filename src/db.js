ec06846b40a845061dca0e610a13410cf85b3104
Cyril Ajieh <>
2017-12-09 15:57:48 -0600

    modified db config

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
+import config from './config';
 
 export default callback => {
-  let db = mongoose.connect('mongodb://localhost:27017/ruralspoon');
+  let db = mongoose.connect(config.mongoUrl);
   callback(db);
 }
